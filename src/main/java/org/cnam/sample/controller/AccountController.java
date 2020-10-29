f6471976820b7b240380d3a0862c9991ca22b086
Thomas Personeni <thomas.personeni0@gmail.com>
2020-10-29 09:39:05 +0100

    Rename route sample -> account

--- src/main/java/org/cnam/sample/controller/AccountController.java
+++ src/main/java/org/cnam/sample/controller/AccountController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/sample")
+@RequestMapping("/account")
 public class AccountController {
 
     @Autowired
